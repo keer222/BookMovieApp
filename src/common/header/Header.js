84d0b5fba929c789e38fec5f37111aa1cb893f3e
Keerthana K <keertha222@gmail.com>
2022-10-23 22:02:30 +0530

    Adjusted contents to center

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -63,134 +63,133 @@ const Header = (props) => {
     }
     return (
         <div className="Header">
-            <img className="Logo" src={logo} alt="Logo"/>
+            <img className="Logo" src={logo} alt="Logo" />
             <Button className="Button-style"
-              variant="contained"
-              onClick={loginButtonHandler}
-              color="default"
+                variant="contained"
+                onClick={loginButtonHandler}
+                color="default"
             >LOGIN </Button>
-            <ReactModal 
+            <ReactModal
                 isOpen={showLoginModal}
                 ariaHideApp={false}
-                contentLabel="Login Modal"
-            ><Tabs value={tabIndex} onChange={handleTabChange} centered>
-              <Tab label="LOGIN" />           
-              <Tab label="REGISTER" />
-            </Tabs>
-            <div className="tab-style">
-                {tabIndex === 0 && (
-                <div>
-                    <FormControl required className="formControl">
-                        <InputLabel htmlFor="username"> Username </InputLabel>
-                        <Input
-                            id="username"
-                            value={username}
-                            type="text"
-                            onChange={setUser}
-                        />                   
-                    </FormControl><br/><br/>
-                    <FormControl required className="formControl">                    
-                        <InputLabel htmlFor="password"> Password </InputLabel>
-                        <Input
-                            id="password"
-                            value={password}
-                            type="password"
-                            onChange={setPwd}
-                        />
-                    </FormControl><br/><br/><br/><br/>
-                    <FormControl required className="formControl">
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            >LOGIN
-                        </Button>
-                </FormControl></div>
-              )}
-            </div>
-            <div className="tab-style">
-                {tabIndex === 1 && (
-                <div>
-                    <FormControl required className="formControl">
-                        <InputLabel htmlFor="firstname"> First Name </InputLabel>
-                        <Input
-                            id="firstname"
-                            value={firstname}
-                            type="text"
-                            onChange={event => {setFirstname(event.target.value)}}
-                        />  
-                        <FormHelperText className={reqFirstname}><span className="red">Required</span></FormHelperText>            
-                    </FormControl><br/><br/>
-                    <FormControl required className="formControl">                    
-                        <InputLabel htmlFor="lastname"> Last Name </InputLabel>
-                        <Input
-                            id="lastname"
-                            value={lastname}
-                            type="text"
-                            onChange={event => {setLastname(event.target.value)}}
-                        />
-                        <FormHelperText className={reqLastname}><span className="red">Required</span></FormHelperText> 
-                    </FormControl><br/><br/>
-                    <FormControl required className="formControl">
-                        <InputLabel htmlFor="email"> Email </InputLabel>
-                        <Input
-                            id="email"
-                            value={email}
-                            type="text"
-                            onChange={event => {setEmail(event.target.value)}}
-                        />  
-                        <FormHelperText className={reqEmail}><span className="red">Required</span></FormHelperText>            
-                    </FormControl><br/><br/>
-                    <FormControl required className="formControl">                    
-                        <InputLabel htmlFor="password"> Password </InputLabel>
-                        <Input
-                            id="password"
-                            value={regPassword}
-                            type="password"
-                            onChange={event => {setRegPassword(event.target.value)}}
-                        />
-                        <FormHelperText className={reqPassword}><span className="red">Required</span></FormHelperText> 
-                    </FormControl><br/><br/>
-                    <FormControl required className="formControl">                    
-                        <InputLabel htmlFor="contact"> Contact No. </InputLabel>
-                        <Input
-                            id="contact"
-                            value={contactno}
-                            type="text"
-                            onChange={event => {setContactno(event.target.value)}}
-                        />
-                        <FormHelperText className={reqContactno}><span className="red">Required</span></FormHelperText> 
-                    </FormControl><br/><br/><br/><br/>
-                    <FormControl required className="formControl">
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={registButtonHandler}
-                            >REGISTER
-                        </Button>
-                </FormControl><br/><br/>
-                {result}</div>
-              )}
-            </div>
-          </ReactModal>
-            
+                contentLabel="Login Modal">
+                <Tabs value={tabIndex} onChange={handleTabChange} centered>
+                    <Tab label="LOGIN" />
+                    <Tab label="REGISTER" />
+                </Tabs>
+                <div className="tab-style">
+                    {tabIndex === 0 && (
+                        <div>
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="username"> Username </InputLabel>
+                                <Input
+                                    id="username"
+                                    value={username}
+                                    type="text"
+                                    onChange={setUser}
+                                />
+                            </FormControl><br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="password"> Password </InputLabel>
+                                <Input
+                                    id="password"
+                                    value={password}
+                                    type="password"
+                                    onChange={setPwd}
+                                />
+                            </FormControl><br /><br /><br /><br />
+                            <FormControl required className="formControl">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                >LOGIN
+                                </Button>
+                            </FormControl></div>
+                    )}
+                </div>
+                <div className="tab-style">
+                    {tabIndex === 1 && (
+                        <div>
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="firstname"> First Name </InputLabel>
+                                <Input
+                                    id="firstname"
+                                    value={firstname}
+                                    type="text"
+                                    onChange={event => { setFirstname(event.target.value) }}
+                                />
+                                <FormHelperText className={reqFirstname}><span className="red">Required</span></FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="lastname"> Last Name </InputLabel>
+                                <Input
+                                    id="lastname"
+                                    value={lastname}
+                                    type="text"
+                                    onChange={event => { setLastname(event.target.value) }}
+                                />
+                                <FormHelperText className={reqLastname}><span className="red">Required</span></FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="email"> Email </InputLabel>
+                                <Input
+                                    id="email"
+                                    value={email}
+                                    type="text"
+                                    onChange={event => { setEmail(event.target.value) }}
+                                />
+                                <FormHelperText className={reqEmail}><span className="red">Required</span></FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="password"> Password </InputLabel>
+                                <Input
+                                    id="password"
+                                    value={regPassword}
+                                    type="password"
+                                    onChange={event => { setRegPassword(event.target.value) }}
+                                />
+                                <FormHelperText className={reqPassword}><span className="red">Required</span></FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="contact"> Contact No. </InputLabel>
+                                <Input
+                                    id="contact"
+                                    value={contactno}
+                                    type="text"
+                                    onChange={event => { setContactno(event.target.value) }}
+                                />
+                                <FormHelperText className={reqContactno}><span className="red">Required</span></FormHelperText>
+                            </FormControl><br /><br /><br /><br />
+                            <FormControl required className="formControl">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={registButtonHandler}
+                                >REGISTER
+                                </Button>
+                            </FormControl><br /><br />
+                            {result}</div>
+                    )}
+                </div>
+            </ReactModal>
+
             <Button className="Button-style"
-              variant="contained"
-              onClick={logoutButtonHandler}
-              color="default"
-            >LOGOUT </Button>
-            <ReactModal 
+                variant="contained"
+                onClick={logoutButtonHandler}
+                color="default">LOGOUT </Button>
+            <ReactModal
                 isOpen={showLogoutModal}
                 ariaHideApp={false}
                 contentLabel="Logout Modal"
             >
             </ReactModal>
-            
+
             <Button className="Button-style"
-              variant="contained"
-              onClick={bookShowButtonHandler}
-              color="primary"
+                variant="contained"
+                onClick={bookShowButtonHandler}
+                color="primary"
             >BOOK SHOW </Button>
-            <ReactModal 
+            <ReactModal
                 isOpen={showBookShowModal}
                 ariaHideApp={false}
                 contentLabel="Book Show Modal"
